188ef8701e1b7b5e23f0b15f6c59ed507fd443c7
hongducdev <contact.hongduc@gmail.com>
2024-02-25 16:58:19 +0700

    Add dark/light mode switch

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,32 @@
-import { createTheme } from "@mui/material/styles";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#556cd6",
+const theme = extendTheme({
+  light: {
+    palette: {
+      primary: {
+        main: "#556cd6",
+      },
+      secondary: {
+        main: "#19857b",
+      },
+      error: {
+        main: red.A400,
+      },
     },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
+  },
+  dark: {
+    palette: {
+      primary: {
+        main: "#90caf9",
+      },
+      secondary: {
+        main: "#f48fb1",
+      },
+      error: {
+        main: red.A400,
+      },
     },
   },
 });
